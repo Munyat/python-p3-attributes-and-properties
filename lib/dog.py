4f3f28664c668d0204045f3eb2384fedcc6a7e9d
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-05-30 23:39:02 +0300

    complete attributes lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-
-APPROVED_BREEDS = [
+class Dog:
+    APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -10,6 +10,29 @@ APPROVED_BREEDS = [
     "Pug",
     "Pointer"
 ]
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
 
-class Dog:
-    pass
+        self.breed = breed
+
+    def get_name(self):
+           return self._name
+    
+    def set_name(self, name):
+        if (type(name) == str) and 1 <= len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+      
+    def set_breed(self, breed):
+        if breed not in self.APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = breed
+    breed = property(get_breed, set_breed)
+
